14b9af10cb44ccb95d1683ca926de2a0a16e805a
Sophie Paton <Paton-S1@ulster.ac.uk>
2024-10-08 11:12:34 +0100

    Created instances and ran program

--- StudentLibrary/Program.cs
+++ StudentLibrary/Program.cs
@@ -1,7 +1,15 @@
-﻿class Program
-{
-    static moid Main(string[] args)
-    {
+﻿using StudentLibrary;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        //create new object for the Student class
+        Student student = new Student();
+        Student student1 = new Student("Jane Doe", 18);
+
+        Console.WriteLine("Current Students: ");
+        student.DisplayInfo();
+        student1.DisplayInfo();
     }
 }
